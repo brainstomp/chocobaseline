1d033ffcca78fb0763defe2c7eb21eda3061fd4c
brainstomp <42446297+brainstomp@users.noreply.github.com>
2022-09-14 11:58:22 -0400

    Create chocobaseline.ps1

---
+++ chocobaseline.ps1
@@ -0,0 +1,24 @@
+#Expand Chocolatey Functionality
+choco install chocolatey-core.extension -y
+
+#Browsers
+choco install firefox -y
+choco install googlechrome -y
+choco install brave -y
+
+#Utilities
+choco install 7zip -y
+choco install notepadplusplus -y
+choco install vlc -y
+choco install foxitreader -y
+choco install sysinternals -y
+choco install putty -y
+choco install pdfcreator -y
+choco install filezilla -y
+choco install irfanview -y
+choco install irfanview-shellextension -y
+choco install irfanviewplugins -y
+
+#Chat
+choco install slack -y
+choco install discord -y
